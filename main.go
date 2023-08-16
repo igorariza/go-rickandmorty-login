702024ec80d2868d2e451411a1dbbf33c4bdfadd
igorariza <igorariza@gmail.com>
2023-08-16 11:35:52 +0200

    update gourutine

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/igorariza/go-rickandmorty-login/api"
 )
 
 func main() {
 
-	go api.MigrateExternalAPI()
+	// go api.MigrateExternalAPI()
 
-	go func(msg string) {
-		fmt.Println(msg)
-	}("Starting migration external API")
+	// go func(msg string) {
+	// 	fmt.Println(msg)
+	// }("Starting migration external API")
 
 	api.Run()
 
